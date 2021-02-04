2865014a5f2249165c513dd1e655c21680d5039c
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-02-04 13:21:45 +0300

    UPDATE: add REST API for User

---
+++ src/main/java/com/blog/models/User.java
@@ -0,0 +1,68 @@
+package com.blog.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer iduser;
+
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+
+    public User(Integer iduser, String name, String surname, String email, String password) {
+        this.iduser = iduser;
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {}
+
+    public Integer getIduser() {
+        return iduser;
+    }
+
+    public void setIduser(Integer iduser) {
+        this.iduser = iduser;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/com/blog/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.blog.repositories;
+
+import com.blog.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+}
