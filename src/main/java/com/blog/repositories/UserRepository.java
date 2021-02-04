e11cfd320a72d6d83c1378606a23b7e7625c5651
vadzimalkhimionak <vad.alkhimenok@gmail.com>
2021-02-04 17:46:44 +0300

    UPDATE: add entiteis

--- src/main/java/com/blog/repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.blog.repositories;
-
-import com.blog.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
